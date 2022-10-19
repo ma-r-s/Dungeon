91ac401af2251e0a3befef8017512e0d53096e2a
Mario Ruiz <m.ruizs@uniandes.edu.co>
2022-10-19 12:41:27 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Dungeon from './dungeon/Dungeon.svelte';
+	let width = 32;
+	let height = 15;
+</script>
+
+<div class="flex flex-col space-y-2 m-3 items-center">
+	<p class="text-2xl font-bold">Grid</p>
+
+	<div>
+		<button
+			class="bg-red-300 rounded-full py-1 px-3 hover:bg-red-500"
+			on:click={() => (height += 1)}>Up</button
+		>
+		<button
+			class="bg-red-300 rounded-full py-1 px-3 hover:bg-red-500"
+			on:click={() => (height -= 1)}>Down</button
+		>
+		<button class="bg-red-300 rounded-full py-1 px-3 hover:bg-red-500" on:click={() => (width += 1)}
+			>Left</button
+		>
+		<button class="bg-red-300 rounded-full py-1 px-3 hover:bg-red-500" on:click={() => (width -= 1)}
+			>Right</button
+		>
+	</div>
+	<Dungeon {width} {height} />
+</div>
---
+++ src/routes/dungeon/Dungeon.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let width = 32;
+	export let height = 18;
+	$: col = `repeat(${width}, 1fr)`;
+	$: row = `repeat(${height}, 1fr)`;
+</script>
+
+<div
+	class="grid divide-x divide-y divide-gray-400"
+	style="grid-template-rows: {row}; grid-template-columns: {col};"
+>
+	{#each Array(height).fill(0) as _, y}
+		{#each Array(width).fill(0) as _, x}
+			<div class="bg-gray-200 w-2 h-2" />
+		{/each}
+	{/each}
+</div>
